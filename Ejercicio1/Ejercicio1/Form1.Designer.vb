569931d2ab0bd73f2d8407648f2b246d823bef62
Freddy Rodriguez <uthekiller1@gmail.com>
2025-02-18 11:08:48 -0400

    add exercise vb

---
+++ Ejercicio1/Ejercicio1/Form1.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtCi = New System.Windows.Forms.TextBox()
+        Me.txtHoras = New System.Windows.Forms.TextBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btnBorrar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtNombre
+        '
+        Me.txtNombre.AccessibleName = ""
+        Me.txtNombre.Location = New System.Drawing.Point(111, 34)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 0
+        '
+        'txtCi
+        '
+        Me.txtCi.Location = New System.Drawing.Point(111, 77)
+        Me.txtCi.Name = "txtCi"
+        Me.txtCi.Size = New System.Drawing.Size(100, 20)
+        Me.txtCi.TabIndex = 1
+        '
+        'txtHoras
+        '
+        Me.txtHoras.Location = New System.Drawing.Point(111, 120)
+        Me.txtHoras.Name = "txtHoras"
+        Me.txtHoras.Size = New System.Drawing.Size(100, 20)
+        Me.txtHoras.TabIndex = 2
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(111, 161)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(100, 20)
+        Me.txtTotal.TabIndex = 3
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(12, 203)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(57, 23)
+        Me.btnCalcular.TabIndex = 4
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'btnBorrar
+        '
+        Me.btnBorrar.Location = New System.Drawing.Point(99, 203)
+        Me.btnBorrar.Name = "btnBorrar"
+        Me.btnBorrar.Size = New System.Drawing.Size(57, 23)
+        Me.btnBorrar.TabIndex = 5
+        Me.btnBorrar.Text = "Borrar"
+        Me.btnBorrar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Nombre:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(20, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "CI:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(90, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Horas trabajadas:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 161)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(73, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Total a pagar:"
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(187, 203)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(57, 23)
+        Me.btnSalir.TabIndex = 10
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(256, 238)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnBorrar)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.txtHoras)
+        Me.Controls.Add(Me.txtCi)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Name = "Form1"
+        Me.Text = "Calculadora de Pagos"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtCi As TextBox
+    Friend WithEvents txtHoras As TextBox
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents btnBorrar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnSalir As Button
+End Class
---
+++ Ejercicio1/Ejercicio1/Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+
+    Private Function CalcularPago(ByVal horasTrabajadas As Integer) As Double
+        Dim total As Double
+
+        If horasTrabajadas <= 40 Then
+            total = horasTrabajadas * 7.5
+        Else
+            total = (40 * 7.5) + ((horasTrabajadas - 40) * 9.5)
+        End If
+
+        Return total
+    End Function
+
+    ' Evento para el botón "Calcular".
+    Private Sub btnCalcular_Click_1(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        ' Intentar capturar las horas trabajadas del TextBox.
+        Try
+            Dim horasTrabajadas As Integer = Convert.ToInt32(txtHoras.Text)
+
+            If horasTrabajadas < 0 Then
+                MessageBox.Show("Las horas trabajadas no pueden ser negativas.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+
+            Dim totalPago As Double = CalcularPago(horasTrabajadas)
+            txtTotal.Text = totalPago.ToString("F2") & " Bs."
+        Catch ex As Exception
+            MessageBox.Show("Por favor, ingrese un número válido para las horas trabajadas.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Evento para el botón "Borrar".
+    Private Sub btnBorrar_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
+        ' Limpiar todos los campos del formulario.
+        txtNombre.Text = ""
+        txtCi.Text = ""
+        txtHoras.Text = ""
+        txtTotal.Text = ""
+    End Sub
+
+    ' Evento para el botón "Salir".
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        ' Cerrar la aplicación.
+        Me.Close()
+    End Sub
+
+End Class
